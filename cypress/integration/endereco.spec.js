fda7e20175a2f18fd26aac9dfc8fb5715676af34
Patrícia Machado <patriciamachado1993@gmail.com>
2021-10-01 20:29:48 -0300

    massa de dados em lista e relatórios

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
-import enderecoPage from '../support/page-objects/endereco.page';
+import enderecoPage from '../support/page-objects/endereco.page'
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     
@@ -11,8 +12,25 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         })
     });
 
-    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+    it ('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFaturamento('Patricia', 'Machado', 'EBAC', 'Brasil', 'Av Brasil', '3100', 'São Paulo', 'São Paulo', '01000100', '11111111', 'email@email.com')    
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it ('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )    
         cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
